6f36506cea2754b5890140c31e68a8d0788fbdff
Isabeli Pinheiro <isabeli.barrr@gmail.com>
2026-09-04 10:08:21 -0300

    refactor: alterações estéticas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main{
   public static void main(String[]args){
-    System.out.println("Sistema de Projetos");
+    System.out.println("===========================");
+    System.out.println("=== SISTEMA DE PROJETOS ===");
+    System.out.println("===========================");
+    System.out.println("= Bem-vindo(a) ao sistema!=");
+
+    System.out.println("");
+    System.out.println("Projeto: Portifólio Acadêmico");
+    System.out.println("Desenvolvido em Java");
+    System.out.println("Versão 1.0");
   }
 }
