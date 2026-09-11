c99c9ce6bf0b4c06fd3e47331053ee5f8fc9d16f
Isabeli Pinheiro <isabeli.barrr@gmail.com>
2026-09-11 10:17:07 -0300

    feat: métodos get e set para os projetos

---
+++ src/model/Projeto.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Projeto {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
